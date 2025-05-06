1d7065ffba338d99991e47176f465c0538d381e3
Victor R. Ravazi <106163330+victorravazi@users.noreply.github.com>
2025-05-06 09:11:01 -0300

    Update ex8.py

--- ListaExercicios2/ex8.py
+++ ListaExercicios2/ex8.py
@@ -1,1 +1,15 @@
 
+
+
+with open("arquivo.txt", "r") as arquivoOrigem, open ("ordemAlfabetica.txt", "w") as arquivoDestino:
+    linhas = [linha.strip() for linha in arquivoOrigem]
+    linhas_ordenadas = sorted(linhas)
+    for linha in linhas_ordenadas:
+        arquivoDestino.write(linha + "\n")
+    
+    
+# Funcionou, arquivo.txt copiado para OrdemAlfabetica.txt
+
+
+
+
